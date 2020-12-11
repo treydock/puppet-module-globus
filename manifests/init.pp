c175d8d5dec3c1c553daa68e33b35a6f2ca34171
Trey Dockendorf <tdockendorf@osc.edu>
2020-12-11 11:37:12 -0500

    Mark which parameters are Globus v4 vs v5

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # @summary Manage Globus
 #
-# @example Install and configure a Globus v5.4
+# @example Install and setup a Globus v5.4 endpoint
 #   class { 'globus':
 #     display_name  => 'REPLACE My Site Globus',
 #     client_id     => 'REPLACE-client-id-from-globus',
@@ -12,154 +12,228 @@
 #   Major version of Globus to install. Only needed to install Globus v4
 # @param include_io_server
 #   Setup Globus v4 IO server
+#   Globus v4 only
 # @param include_id_server
 #   Setup Globus v4 ID server
+#   Globus v4 only
 # @param include_oauth_server
 #   Setup Globus v4 OAuth server
+#   Globus v4 only
 # @param release_url
 #   Release URL of Globus release RPM
+#   Globus v4 & v5
 # @param repo_baseurl
 #   Globus RPM repo baseurl
+#   Globus v4 & v5
 # @param repo_testing_baseurl
 #   Globus testing RPM repo baseurl
+#   Globus v4 & v5
 # @param repo_baseurl_v5
 #   Globus v5 repo baseurl
+#   Globus v4 & v5
 # @param repo_testing_baseurl_v5
 #   Globus v5 testing repo baseurl
+#   Globus v4 & v5
 # @param extra_gridftp_settings
 #   Additional settings for GridFTP
+#   Globus v4 & v5
 # @param first_gridftp_callback
 #   Used when running GridFTP from Globus with OSG, see README.
+#   Globus v4 only
 # @param manage_service
 #   Boolean to set if globus-gridftp-server service is managed
+#   Globus v4 & v5
 # @param run_setup_commands
 #   Boolean to set if the commands to setup Globus are run (v4 and v5)
+#   Globus v4 & v5
 # @param manage_firewall
 #   Boolean to set if firewall rules are managed by this module
+#   Globus v4 & v5
 # @param manage_epel
 #   Boolean to set if EPEL is managed by this repo
+#   Globus v4 & v5
 # @param repo_dependencies
 #   Additional repo dependencies
+#   Globus v4 only
 # @param manage_user
 #   Boolean to set if the gcsweb user and group are managed by this module
+#   Globus v5 only
 # @param group_gid
 #   The gcsweb group GID
+#   Globus v5 only
 # @param user_uid
 #   The gcsweb user UID
+#   Globus v5 only
 # @param package_name
 #   Globus v5 package name
 # @param display_name
 #   Display name to use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param client_id
 #   --client-id use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param client_secret
 #   --client-secret use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param owner
 #   --owner use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param deployment_key
 #   --deployment-key use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param organization
 #   --organization use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param keywords
 #   --keywords use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param department
 #   --department use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param contact_email
 #   --contact-email use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param contact_info
 #   --contact-info use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param info_link
 #   --info-link use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param description
 #   --description use when running 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param public
 #   When false pass --private flag to 'globus-connect-server endpoint setup'
+#   Globus v5 only
 # @param incoming_port_range
 #   --incoming-port-range use when running 'globus-connect-server node setup'
+#   Globus v5 only
 # @param outgoing_port_range
 #   --outgoing-port-range use when running 'globus-connect-server node setup'
+#   Globus v5 only
 # @param ip_address
 #   --ip-address use when running 'globus-connect-server node setup'
+#   Globus v5 only
 # @param export_node
 #   --export-node use when running 'globus-connect-server node setup'
+#   Globus v5 only
 # @param import_node
 #   --import-node use when running 'globus-connect-server node setup'
+#   Globus v5 only
 # @param globus_user
 #   See globus-connect-server.conf Globus/User
+#   Globus v4 only
 # @param globus_password
 #   See globus-connect-server.conf Globus/Password
+#   Globus v4 only
 # @param endpoint_name
 #   See globus-connect-server.conf Endpoint/Name
+#   Globus v4 only
 # @param endpoint_public
 #   See globus-connect-server.conf Endpoint/Public
+#   Globus v4 only
 # @param endpoint_default_directory
 #   See globus-connect-server.conf Endpoint/DefaultDirectory
+#   Globus v4 only
 # @param security_fetch_credentials_from_relay
 #   See globus-connect-server.conf Security/FetchCredentialFromRelay
+#   Globus v4 only
 # @param security_certificate_file
 #   See globus-connect-server.conf Security/CertificateFile
+#   Globus v4 only
 # @param security_key_file
 #   See globus-connect-server.conf Security/KeyFile
+#   Globus v4 only
 # @param security_trusted_certificate_directory
 #   See globus-connect-server.conf Security/TrustedCertificateDirectory
+#   Globus v4 only
 # @param security_identity_method
 #   See globus-connect-server.conf Security/IdentityMethod
+#   Globus v4 only
 # @param security_authorization_method
 #   See globus-connect-server.conf Security/AuthorizationMethod
+#   Globus v4 only
 # @param security_gridmap
 #   See globus-connect-server.conf Security/Gridmap
+#   Globus v4 only
 # @param security_cilogon_identity_provider
 #   See globus-connect-server.conf Security/IdentityProvider
+#   Globus v4 only
 # @param gridftp_server
 #   See globus-connect-server.conf GridFTP/Server
+#   Globus v4 only
 # @param gridftp_server_port
 #   See globus-connect-server.conf GridFTP/ServerPort
+#   Globus v4
 # @param gridftp_server_behind_nat
 #   See globus-connect-server.conf GridFTP/ServerBehindNat
+#   Globus v4 only
 # @param gridftp_incoming_port_range
 #   See globus-connect-server.conf GridFTP/IncomingPortRange
+#   Globus v4 only
 # @param gridftp_outgoing_port_range
 #   See globus-connect-server.conf GridFTP/OutgoingPortRange
+#   Globus v4 only
 # @param gridftp_data_interface
 #   See globus-connect-server.conf GridFTP/DataInterface
+#   Globus v4 only
 # @param gridftp_restrict_paths
 #   See globus-connect-server.conf GridFTP/RestrictPaths
+#   Globus v4 only
 # @param gridftp_sharing
 #   See globus-connect-server.conf GridFTP/Sharing
+#   Globus v4 only
 # @param gridftp_sharing_restrict_paths
 #   See globus-connect-server.conf GridFTP/SharingRestrictPaths
+#   Globus v4 only
 # @param gridftp_sharing_state_dir
 #   See globus-connect-server.conf GridFTP/SharingStateDir
+#   Globus v4 only
 # @param gridftp_sharing_users_allow
 #   See globus-connect-server.conf GridFTP/UsersAllow
+#   Globus v4 only
 # @param gridftp_sharing_groups_allow
 #   See globus-connect-server.conf GridFTP/GroupsAllow
+#   Globus v4 only
 # @param gridftp_sharing_users_deny
 #   See globus-connect-server.conf GridFTP/UsersDeny
+#   Globus v4 only
 # @param gridftp_sharing_groups_deny
 #   See globus-connect-server.conf GridFTP/GroupsDeny
+#   Globus v4 only
 # @param myproxy_server
 #   See globus-connect-server.conf MyProxy/Server
+#   Globus v4 only
 # @param myproxy_server_port
 #   See globus-connect-server.conf MyProxy/ServerPort
+#   Globus v4 only
 # @param myproxy_server_behind_nat
 #   See globus-connect-server.conf MyProxy/ServerBehindNAT
+#   Globus v4 only
 # @param myproxy_ca_directory
 #   See globus-connect-server.conf MyProxy/CADirectory
+#   Globus v4 only
 # @param myproxy_config_file
 #   See globus-connect-server.conf MyProxy/ConfigFile
+#   Globus v4 only
 # @param myproxy_ca_subject_dn
 #   See globus-connect-server.conf MyProxy/CaSubjectDN
+#   Globus v4 only
 # @param myproxy_firewall_sources
 #   Sources to open in firewall for MyProxy
+#   Globus v4 only
 # @param oauth_server
 #   See globus-connect-server.conf OAuth/Server
+#   Globus v4 only
 # @param oauth_server_behind_firewall
 #   See globus-connect-server.conf OAuth/ServerBehindFirewall
+#   Globus v4 only
 # @param oauth_stylesheet
 #   See globus-connect-server.conf OAuth/Stylesheet
+#   Globus v4 only
 # @param oauth_logo
 #   See globus-connect-server.conf OAuth/Logo
+#   Globus v4 only
 #
 class globus (
   Variant[Enum['4','5'],Integer[4,5]] $version = '5',
