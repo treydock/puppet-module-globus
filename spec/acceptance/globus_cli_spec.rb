9954905b5bfd319c81f459f32f808fcdd48ef102
Trey Dockendorf <tdockendorf@osc.edu>
2021-09-07 10:22:36 -0400

    Support installing globus-timer-cli

--- spec/acceptance/globus_cli_spec.rb
+++ spec/acceptance/globus_cli_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper_acceptance'
 describe 'globus::cli class:', unless: fact('os.release.major') == '20.04' do
   context 'with default parameters' do
     it 'runs successfully' do
-      pp = 'include globus::cli'
+      pp = "class { 'globus::cli': timer_ensure => 'present' }"
 
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
 
     describe command('globus --version') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+    describe command('globus-timer --version') do
       its(:exit_status) { is_expected.to eq 0 }
     end
   end
