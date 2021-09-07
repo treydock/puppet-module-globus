55d9abde1cd2daec3813c498922467a6e7584194
Trey Dockendorf <tdockendorf@osc.edu>
2021-09-07 12:05:19 -0400

    globus-timer-cli is not able to install on Debian 9

--- spec/acceptance/globus_cli_spec.rb
+++ spec/acceptance/globus_cli_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper_acceptance'
 describe 'globus::cli class:', unless: fact('os.release.major') == '20.04' do
   context 'with default parameters' do
     it 'runs successfully' do
-      pp = "class { 'globus::cli': timer_ensure => 'present' }"
+      # Python is too old on Debian 9 for globus-timer-cli
+      timer_ensure = if fact('os.name') == 'Debian' && fact('os.release.major') == '9'
+                       'absent'
+                     else
+                       'present'
+                     end
+      pp = "class { 'globus::cli': timer_ensure => '#{timer_ensure}' }"
 
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
@@ -12,7 +18,7 @@ describe 'globus::cli class:', unless: fact('os.release.major') == '20.04' do
     describe command('globus --version') do
       its(:exit_status) { is_expected.to eq 0 }
     end
-    describe command('globus-timer --version') do
+    describe command('globus-timer --version'), unless: (fact('os.family') == 'Debian' && ['9', '20.04'].include?(fact('os.release.major'))) do
       its(:exit_status) { is_expected.to eq 0 }
     end
   end
