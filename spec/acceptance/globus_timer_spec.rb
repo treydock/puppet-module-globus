b1cd2c2444048ffeb51591c97d6359081193de45
treydock <tdockendorf@osc.edu>
2022-04-05 11:03:45 -0400

    Move globus-timer-cli into dedicated class to avoid conflicts with globus-cli (#30)

--- spec/acceptance/globus_timer_spec.rb
+++ spec/acceptance/globus_timer_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper_acceptance'
 
-describe 'globus::cli class:' do
+describe 'globus::timer class:', unless: fact('os.name') == 'Debian' && fact('os.release.major') == '9' do
   context 'with default parameters' do
     it 'runs successfully' do
-      pp = 'include globus::cli'
+      pp = 'include globus::timer'
 
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
 
-    describe command('globus version') do
+    describe command('globus-timer --version') do
       its(:exit_status) { is_expected.to eq 0 }
     end
   end
