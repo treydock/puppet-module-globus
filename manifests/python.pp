f9a86ef11913aac12fc71b0828fb50b41badc5a7
Trey Dockendorf <tdockendorf@osc.edu>
2022-08-09 16:56:14 -0400

    Only do venv logic on osfamily=Debian

--- manifests/python.pp
+++ manifests/python.pp
@@ -4,11 +4,12 @@ class globus::python (
   String $version = '3',
   Array $package_dependencies = [],
   String $pip_provider = 'pip',
+  Optional[String] $venv_ensure = undef,
   String $venv_python_version = 'system',
 ) {
   class { 'python':
     version => $version,
     dev     => 'present',
-    venv    => 'present',
+    venv    => $venv_ensure,
   }
 }
