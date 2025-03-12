28041adb5eba35d789694b6db737187e4c2960ca
treydock <tdockendorf@osc.edu>
2025-03-12 09:54:32 -0400

    Support Debian 12 (#46)
    * Support Debian 12
    
    * Fix unit and acceptance tests for Debian 12

--- spec/spec_helper_acceptance_setup.rb
+++ spec/spec_helper_acceptance_setup.rb
@@ -3,6 +3,10 @@
 RSpec.configure do |c|
   c.before(:suite) do
     on hosts, 'mkdir -p /opt/puppetlabs/puppet/cache/lib/facter/'
-    on hosts, 'cp /etc/puppetlabs/code/environments/production/modules/python/lib/facter/* /opt/puppetlabs/puppet/cache/lib/facter/'
+    if ['debian-12', 'ubuntu-2404'].include?(ENV['BEAKER_set'])
+      on hosts, 'cp /etc/puppet/code/modules/python/lib/facter/* /opt/puppetlabs/puppet/cache/lib/facter/'
+    else
+      on hosts, 'cp /etc/puppetlabs/code/environments/production/modules/python/lib/facter/* /opt/puppetlabs/puppet/cache/lib/facter/'
+    end
   end
 end
