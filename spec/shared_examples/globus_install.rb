b9c3348c873e38b7206f3461f821868021966f51
Trey Dockendorf <tdockendorf@osc.edu>
2025-03-12 08:27:08 -0400

    Fix unit tests

--- spec/shared_examples/globus_install.rb
+++ spec/shared_examples/globus_install.rb
@@ -5,7 +5,7 @@ shared_examples_for 'globus::install' do |facts|
   it { is_expected.not_to contain_package('globus-connect-server-id') }
   it { is_expected.not_to contain_package('globus-connect-server-web') }
 
-  if facts[:os]['family'] == 'RedHat' && facts[:os]['release']['major'].to_i >= 8
+  if facts[:os]['family'] == 'RedHat' && facts[:os]['release']['major'].to_i == 8
     it do
       is_expected.to contain_package('mod_auth_openidc-dnf-module').with(
         ensure: 'disabled',
@@ -14,6 +14,8 @@ shared_examples_for 'globus::install' do |facts|
         before: 'Package[globus-connect-server54]',
       )
     end
+  else
+    it { is_expected.not_to contain_package('mod_auth_openidc-dnf-module') }
   end
   it { is_expected.to contain_package('globus-connect-server54').with_ensure('present') }
 end
